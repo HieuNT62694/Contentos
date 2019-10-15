1fb836e41e637d512ab1b021dd26ce787b969c0a
Hiếu Nguyễn <hieuntse62694@fpt.edu.vn>
2019-10-15 17:16:39 +0700

    [AuthenService] create Api Update + Fix Api login

---
+++ AuthenticationService/AuthenticationService/Application/Commands/UpdateCustomer/UpdateCustomerAccountCommads.cs
@@ -0,0 +1,23 @@
+﻿using AuthenticationService.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Application.Commands.UpdateCustomer
+{
+    public class UpdateCustomerAccountCommads : IRequest<CreateUserModel>
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+    }
+}
